b43a13d59477d231bc9a150f4b8d909d23b950a5
Frantisek Drabecky <frantisek.drabecky@microstep-mis.com>
2017-02-15 17:48:49 +0100

    vytvorenie novej sablony
    zobrazenie jednotlivych post_view po kliknuti na Title

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    post_id = Post.objects.filter(id=3)
+    return render(request, 'blog/post_list.html', {'posts': posts,
+                                                   'post_id': post_id})
 
+def post_detail(request, pk):
+     post = get_object_or_404(Post, pk=pk)
+     return render(request, 'blog/post_detail.html', {'post': post})
